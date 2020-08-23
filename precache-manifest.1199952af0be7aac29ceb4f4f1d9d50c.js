caaf5b48681631f81226fd17e4a9a9ea23d73b14
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-23 10:52:26 +0530

    Updates

---
+++ precache-manifest.1199952af0be7aac29ceb4f4f1d9d50c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fab000ce2f5ee4f19386f541fbbfee1",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "224ea9421fa8ee715b22",
+    "url": "/react-covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "e897cb20dfe70c9902b0",
+    "url": "/react-covid-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "224ea9421fa8ee715b22",
+    "url": "/react-covid-tracker/static/js/2.29d7cea3.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-covid-tracker/static/js/2.29d7cea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e897cb20dfe70c9902b0",
+    "url": "/react-covid-tracker/static/js/main.f4f8c94f.chunk.js"
+  },
+  {
+    "revision": "261fd4b71819be31f984",
+    "url": "/react-covid-tracker/static/js/runtime-main.803e8507.js"
+  }
+]);
